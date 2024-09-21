8b4ccd3a1e43355fb7453e4a7e075f31c2cad33b
Michael Angelo Manaog <michael.manaog@teamhgs.com>
2024-09-22 04:11:36 +0800

    add testing in auth web

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -1,8 +1,53 @@
 <?php
 
-test('a user can login with valid credentials', function () {
-    
+use App\Models\User;
+use function Pest\Laravel\post;
 
-    expect(true)->toBeTrue();
+test('a user can login with valid credentials and redirects to dashboard', function () {
+    $user = User::factory()->create();
+
+    $response = post(route('login'), [
+        'email' => $user->email,
+        'password' => 'password'
+    ]);
+
+    $response->assertRedirect(route('dashboard'));
+    $this->assertAuthenticatedAs($user);
 });
 
+test('a user cannot login with invalid credentials', function () {
+    $response = post(route('login'), [
+        'email' => 'johndoe@gmail.com',
+        'password' => 'password'
+    ]);
+
+    $this->assertGuest();
+    $response->assertSessionHasErrors(['email']);
+});
+
+test('a user with correct inputs can register and redirects to dashboard', function() {
+    $userData = [
+        'name' => fake()->name(),
+        'email' => fake()->unique()->safeEmail(),
+        'password' => 'password',
+        'password_confirmation' => 'password', 
+    ];
+
+    $response = post(route('register'), $userData);
+
+    $response->assertRedirect(route('dashboard'));
+    $this->assertAuthenticated();
+});
+
+
+test('a user cannot register with invalid inputs', function () {
+    $response = post(route('register'), [
+        'name' => '',
+        'email' => 'wrongemail',
+        'password' => 'password',
+        'password_confirmation' => 'new_password',
+    ]);
+
+    $this->assertGuest();
+    $response->assertSessionHasErrors(['name', 'email', 'password']);
+});
